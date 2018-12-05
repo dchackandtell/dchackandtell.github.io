ed9644f8b4ad159103c4095513ddc2b493641d28
Travis Hoppe <travis.hoppe@gmail.com>
2018-12-05 16:37:48 -0500

    Added links for round 62

--- markdownify.py
+++ markdownify.py
@@ -11,10 +11,17 @@ f = sys.argv[1]
 with open(f) as FIN:
     raw = FIN.read()
 
-PAT = re.compile(ur'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))')
+PAT = re.compile(r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))')
+
+#for line in raw.split('\n'):
+#    print (line)
+#    print (PAT.sub(r"[\1](\1)", line))
 
 parsed = PAT.sub(r"[\1](\1)", raw)
+
 with open(f, 'w') as FIN:
     FIN.write(parsed)
 
 print("Output written to %s"%f)
+
+# Notes copied from the [hackpad]([https://docs.google.com/document/d/1DM_qVHEGPNP-UzSGKNlee8lmroNqap4Fg4RgERxxTiY/edit#](https://docs.google.com/document/d/1DM_qVHEGPNP-UzSGKNlee8lmroNqap4Fg4RgERxxTiY/edit#))!
